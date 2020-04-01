1cebb0f3cfbcf210a84223979a18f08d2b335ea2
Asiya Eli <asiyae@outlook.com>
2020-04-01 15:07:39 -0400

    update confilict

--- src/test/java/com/cybertek/tests/GitPractice.java
+++ src/test/java/com/cybertek/tests/GitPractice.java
@@ -1,9 +1,18 @@
 package com.cybertek.tests;
 
+import org.testng.annotations.Test;
+
+import java.lang.annotation.Target;
+
 public class GitPractice {
   //Hi Arman 
   //this is the code from your co-worker, and you do not aware of that 
   
-  //you pushed it frist , then tried to update your code. 
+  //you pushed it frist , then tried to update your code.
+
+    @Test
+    public void test1(){
+        System.out.println("hi ");
+    }
 
 }
