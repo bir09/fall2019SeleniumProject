fdb874662463b103d670ce79bde1f57567cff4af
“Namra” <180696arman@mail.ru>
2020-04-01 15:27:51 -0400

    branch

---
+++ src/test/java/com/cybertek/tests/GitAsiyaBranch.java
@@ -0,0 +1,10 @@
+package com.cybertek.tests;
+
+import org.testng.annotations.Test;
+
+public class GitAsiyaBranch {
+    @Test
+    public void test(){
+        System.out.println("cancel vehicle");
+    }
+}
