c7e67d37cdf736700a7d5f2b0745571254badaa8
“Namra” <180696arman@mail.ru>
2020-04-01 15:07:13 -0400

    update

--- src/test/java/com/cybertek/tests/GitPractice.java
+++ src/test/java/com/cybertek/tests/GitPractice.java
@@ -1,9 +1,17 @@
 package com.cybertek.tests;
 
+import org.testng.annotations.Test;
+
 public class GitPractice {
   //Hi Arman 
   //this is the code from your co-worker, and you do not aware of that 
   
   //you pushed it frist , then tried to update your code. 
 
+
+    @Test
+    public void test2(){
+        System.out.println("HI Asiya");
+
+    }
 }
